975107d72b33258de139c6a9e72272b7473baf3c
titangamerpro <titangamerpro10@gmail.com>
2025-07-15 13:44:43 +0200

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "./Nav";
-
+import { IoSunnyOutline } from "react-icons/io5";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState( () => {
+    const saved = localStorage.getItem('theme')
+    if (saved === 'dark') return true
+    if (saved === 'light') return false
+  });
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    const usePreferDark = window.matchMedia("prefers-color-scheme:dark").matches
+    const dark = savedTheme === "dark" || (!savedTheme && usePreferDark)
+    setIsActive(dark)
+  },[])
+
+  useEffect(() => {
+    const root = document.body
+    if (isActive){
+      root.classList.add('dark')
+      localStorage.setItem('theme', 'dark')
+    }
+    else{
+      root.classList.remove('dark')
+      localStorage.setItem('theme', 'light')
+    }
+  }, [isActive])
+
+  const toggleTheme = () => setIsActive(prev => !prev)
+  
+
   return (
     <header className="header">
       <div className="header-fixed">
@@ -11,9 +40,11 @@ const Header = () => {
           <a href="" className="header-logo">
             Michel
           </a>
-            <Nav/>
+          <Nav />
           <div className="header-block">
-            <button className="btn swich"></button>
+            <button className="btn swich" onClick={toggleTheme}>
+              {isActive ? <IoSunnyOutline/> : <FaMoon/>}
+            </button>
             <a href="" className="header-link">
               Resume
             </a>
